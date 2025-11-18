1fd110f15f522dc7658708202b6edbe1d6802940
Neena Varughese <158041703+neenavarughese55@users.noreply.github.com>
2025-11-18 15:20:06 -0600

    variable data structure

--- solve.py
+++ solve.py
@@ -8,7 +8,7 @@ import time
 class Variable:
     def __init__(self, name, direction, start_pos, length):
         self.name = name
-        self.direction = direction
-        self.start_pos = start_pos
-        self.length = length
-        self.domain = []
+        self.direction = direction # 'across' or 'down'
+        self.start_pos = start_pos # (row, col) of starting square
+        self.length = length 
+        self.domain = [] # list of possible words
