712fcf54f1c3814d2e7a7d19c5c51d2ba9f87193
Neena Varughese <158041703+neenavarughese55@users.noreply.github.com>
2025-11-18 15:31:52 -0600

    Constraint data structure

--- solve.py
+++ solve.py
@@ -10,5 +10,12 @@ class Variable:
         self.name = name
         self.direction = direction # 'across' or 'down'
         self.start_pos = start_pos # (row, col) of starting square
-        self.length = length 
+        self.length = length # To find out the word length
         self.domain = [] # list of possible words
+
+class Constraint:
+    def __init__(self, var1, var2, pos1, pos2):
+        self.var1 = var1 # first variable
+        self.var2 = var2 # second variable
+        self.pos1 = pos1 # position in var1 where they intersect
+        self.pos2 = pos2 # position in var2 where they intersect
